78ff03f0dae883e5ab6342f48492cd7340f0cd6e
Ivan Mondragon <gendou_ika@hotmail.com>
2020-11-20 15:59:56 -0600

    Initial commit

---
+++ Persistence/Migrations/20201120014715_SeedValues2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValues2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] {"Id", "Name"},
+                values: new object[] {4, "value 104"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] {"Id", "Name"},
+                values: new object[] {5, "value 105"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] {"Id", "Name"},
+                values: new object[] {6, "value 106"}
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
